72d91159992c2cafed6dcb3ec474c112eba0341f
Hugo Snihs <hugo.snihs@hotmail.com>
2025-02-25 16:52:42 +0100

    Nu med dagräknare(räknar antal loops), enkelt poängsystem för intill-liggande hus, och koordinat-verifierare

--- main.js
+++ main.js
@@ -15,16 +15,45 @@ function display_map(map) {
 function main() {
     var size_x = 5;
     var size_y = 5;
-    var my_map = (0, maps_1.create_map)(size_x, size_y);
+    var game_map = (0, maps_1.create_map)(size_x, size_y);
     var game_running = true;
+    var game_turn = 0;
+    var game_points = 0;
     var prompt = promptSync();
     while (game_running) {
-        display_map(my_map);
+        display_map(game_map);
+        console.log("Day: ".concat(game_turn));
+        console.log("Points: ".concat(game_points));
         var user_coordinates = prompt('Enter coordinate of choosing: ');
         var user_building = prompt('Enter your building of choosing: ');
         var _a = user_coordinates.split(',').map(Number), x = _a[0], y = _a[1];
-        (0, maps_1.change_property)(my_map, (0, list_1.pair)(x, y), user_building[0].toUpperCase());
+        if (x < 0 || y < 0 || x >= size_x || y >= size_y || isNaN(x) || isNaN(y)) {
+            console.log(" *** Invalid coordinates, try again! *** ");
+            continue;
+        }
+        if (isNaN(parseInt(user_building))) {
+            console.log("bad type of building!");
+            continue;
+        }
+        var adjacent_tiles = [
+            x + 1 < size_x ? (0, list_1.pair)(x + 1, y) : null,
+            x - 1 >= 0 ? (0, list_1.pair)(x - 1, y) : null,
+            y + 1 < size_y ? (0, list_1.pair)(x, y + 1) : null,
+            y - 1 >= 0 ? (0, list_1.pair)(x, y - 1) : null,
+        ].filter(function (n) { return n !== null; }); //Kollar så att närliggande tiles är inom gränserna av spelet,
+        //tar bort dem som ej är det(null).
+        (0, maps_1.change_property)(game_map, (0, list_1.pair)(x, y), user_building[0].toUpperCase());
+        var neighbors = 0;
+        for (var _i = 0, adjacent_tiles_1 = adjacent_tiles; _i < adjacent_tiles_1.length; _i++) {
+            var neighbor = adjacent_tiles_1[_i];
+            if ((0, maps_1.get_property)(game_map, neighbor) === "H") {
+                neighbors += 1;
+            }
+        }
+        game_points += neighbors;
         console.log("You have built a ".concat(user_building, " at coordinate ").concat(user_coordinates, "!"));
+        game_turn += 1;
+        console.log("-----------------------------------------"); // För synlighet i terminalen mellan dagar(turns)
     }
 }
 main();
--- main.ts
+++ main.ts
@@ -17,23 +17,52 @@ function display_map(map: Map): void {
 function main(): void {
     const size_x = 5;
     const size_y = 5;
-    let my_map = create_map(size_x, size_y);
+    let game_map = create_map(size_x, size_y);
     let game_running: boolean = true;
+    let game_turn = 0;
+    let game_points = 0;
             
     const prompt = promptSync();
 
 
     while (game_running) {
-        display_map(my_map);
+        display_map(game_map);
+        console.log(`Day: ${game_turn}`)
+        console.log(`Points: ${game_points}`)
 
         const user_coordinates = prompt('Enter coordinate of choosing: ');
         const user_building = prompt('Enter your building of choosing: ');
 
         const [x, y] = user_coordinates.split(',').map(Number);
 
-        change_property(my_map, pair(x, y), user_building[0].toUpperCase());
+        if (x < 0 || y < 0 || x >= size_x || y >= size_y || isNaN(x) || isNaN(y)) {
+            console.log(" *** Invalid coordinates, try again! *** ");
+            continue;
+        }
+        
+        const adjacent_tiles = [
+                            x + 1 < size_x ? pair(x + 1, y) : null,
+                            x - 1 >= 0     ? pair(x - 1, y) : null, 
+                            y + 1 < size_y ? pair(x, y + 1) : null, 
+                            y - 1 >= 0     ? pair(x, y - 1) : null, 
+                          ].filter(n => n !== null); //Kollar så att närliggande tiles är inom gränserna av spelet,
+                                                     //tar bort dem som ej är det(null).
+
+        change_property(game_map, pair(x, y), user_building[0].toUpperCase());
+
+        let neighbors = 0;
+        for (let neighbor of adjacent_tiles) {
+            if (get_property(game_map, neighbor) === "H"){
+                neighbors += 1;
+            }
+        }
+
+        game_points += neighbors;
 
         console.log(`You have built a ${user_building} at coordinate ${user_coordinates}!`);
+
+        game_turn += 1;
+        console.log(`-----------------------------------------`) // För synlighet i terminalen mellan dagar(turns)
     }
 }
 
