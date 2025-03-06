c608af47c9d74e1e16fe022bd69b8f4a845ee1ce
Hugo Snihs <hugo.snihs@hotmail.com>
2025-03-06 20:50:09 +0100

    nu 3 testcases totalt

--- tiletopia.test.ts
+++ tiletopia.test.ts
@@ -7,7 +7,6 @@ test('Barbarians spawn on single tile', () => {
     const my_map = create_map(x, y);
     change_property(my_map, [0, 0], "E")
     change_property(my_map, [0, 0], "E");
-
     spawn_barbarian(my_map);
     expect(my_map[0][0].property).toStrictEqual("B")});
 
@@ -19,6 +18,15 @@ test('Fortress clears surrounding tiles of barbarians', () => {
     change_property(my_map, [0, 0], "H");
     upgrade_to_fortress(my_map, [0, 0], 3);
     expect(my_map[1][0].property).toStrictEqual("E")});
-
-
     
+test('Placing a house, a church and connecting them with a road. Then counting points', () => {
+    const x = 4
+    const y = 4
+    const my_map = create_map(x, y)
+    change_property(my_map, [0, 1], "E");
+    change_property(my_map, [1, 1], "E");
+    change_property(my_map, [2, 1], "E");
+    place(my_map, [0, 1], "House")
+    place(my_map, [1, 1], "Road")
+    place(my_map, [2, 1], "Church")
+    expect(count_total_points(my_map)).toEqual(2)});
